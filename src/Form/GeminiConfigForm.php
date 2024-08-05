dc9b1632884cd6aeeffb88b93f8f69ffff75998f
Giorgi Jibladze <giorgi@omedia.ge>
2024-08-05 12:22:13 +0400

    feat: add config form and implement basic plugin.

---
+++ src/Form/GeminiConfigForm.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\gemini_provider\Form;
+
+use Drupal\ai\AiProviderPluginManager;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure Gemini Provider settings.
+ */
+final class GeminiConfigForm extends ConfigFormBase {
+
+  /**
+   * Config settings.
+   */
+  const CONFIG_NAME = 'gemini_provider.settings';
+
+  /**
+   * The AI provider manager.
+   *
+   * @var \Drupal\ai\AiProviderPluginManager
+   */
+  protected $aiProviderManager;
+
+  /**
+   * Module Handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Constructs a new AnthropicConfigForm object.
+   */
+  final public function __construct(AiProviderPluginManager $ai_provider_manager, ModuleHandlerInterface $module_handler) {
+    $this->aiProviderManager = $ai_provider_manager;
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  final public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('ai.provider'),
+      $container->get('module_handler')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'gemini_provider_gemini_config';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['gemini_provider.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config(static::CONFIG_NAME);
+
+    $form['api_key'] = [
+      '#type' => 'key_select',
+      '#title' => $this->t('Gemini API Key'),
+      '#description' => $this->t('The Gemini API Key.'),
+      '#default_value' => $config->get('api_key'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $this->config('gemini_provider.settings')
+      ->set('api_key', $form_state->getValue('api_key'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/Plugin/AiProvider/GeminiProvider.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace Drupal\gemini_provider\Plugin\AiProvider;
+
+use Drupal\ai\Attribute\AiProvider;
+use Drupal\ai\Base\AiProviderClientBase;
+use Drupal\ai\Exception\AiResponseErrorException;
+use Drupal\ai\OperationType\Chat\ChatInput;
+use Drupal\ai\OperationType\Chat\ChatInterface;
+use Drupal\ai\OperationType\Chat\ChatMessage;
+use Drupal\ai\OperationType\Chat\ChatOutput;
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Gemini\Client;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Plugin implementation of the Google's Gemini.
+ */
+#[AiProvider(
+  id: 'gemini',
+  label: new TranslatableMarkup('Gemini')
+)]
+class GeminiProvider extends AiProviderClientBase implements ChatInterface {
+
+  /**
+   * The Gemini Client.
+   *
+   * @var Client|null
+   */
+  protected $client;
+
+  /**
+   * API Key.
+   *
+   * @var string
+   */
+  protected string $apiKey = '';
+
+  /**
+   * Run moderation call, before a normal call.
+   *
+   * @var boolean
+   */
+  protected bool $moderation = TRUE;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfiguredModels(string $operation_type = NULL): array {
+    $this->loadClient();
+
+    $supported_models = [];
+    try {
+      $models = $this->client->models()->list()->toArray();
+
+      if (!empty($models['models'])) {
+        foreach ($models['models'] as $model) {
+          $supported_models[$model['name']] = $model['displayName'];
+        }
+      }
+    }
+    catch (\JsonException $e) {
+      throw new AiResponseErrorException('Couldn\'t fetch gemini models.');
+    }
+
+    return $supported_models;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isUsable(string $operation_type = NULL): bool {
+    if (!$this->getConfig()->get('api_key')) {
+      return FALSE;
+    }
+
+    if ($operation_type) {
+      return in_array($operation_type, $this->getSupportedOperationTypes());
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSupportedOperationTypes(): array {
+    //TODO: We need to add other operation types here later.
+    return ['chat'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(): ImmutableConfig {
+    return $this->configFactory->get('gemini_provider.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getApiDefinition(): array {
+    $definition = Yaml::parseFile(
+      $this->moduleHandler->getModule('gemini_provider')
+        ->getPath() . '/definitions/api_defaults.yml'
+    );
+    return $definition;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getModelSettings(string $model_id, array $generalConfig = []): array {
+    return $generalConfig;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAuthentication(mixed $authentication): void {
+    $this->apiKey = $authentication;
+    $this->client = NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function chat(array|string|ChatInput $input, string $model_id, array $tags = []): ChatOutput {
+    $this->loadClient();
+
+    $chat_input = $input;
+    $system_prompt = '';
+    if ($input instanceof ChatInput) {
+      $chat_input = [];
+      foreach ($input->getMessages() as $message) {
+        if ($message->getRole() == 'system') {
+          $system_prompt = $message->getText();
+          continue;
+        }
+
+        $chat_input[] = [
+          'role' => $message->getRole(),
+          'content' => $message->getText(),
+        ];
+      }
+    }
+
+    $payload = ([
+        'model' => $model_id,
+        'messages' => $chat_input,
+      ] + $this->configuration);
+    if (!isset($payload['system']) && $system_prompt) {
+      $payload['system'] = $system_prompt;
+    }
+
+    $response = $this->client->generativeModel($payload['model'])
+      ->generateContent($payload['messages'][0]['content']);
+
+    $text = '';
+    if (!empty($response->parts())) {
+      $text = $response->text();
+    }
+
+    $message = new ChatMessage('', $text);
+
+    return new ChatOutput($message, $response, []);
+  }
+
+  /**
+   * Enables moderation response, for all next coming responses.
+   */
+  public function enableModeration(): void {
+    $this->moderation = TRUE;
+  }
+
+  /**
+   * Disables moderation response, for all next coming responses.
+   */
+  public function disableModeration(): void {
+    $this->moderation = FALSE;
+  }
+
+  /**
+   * Gets the raw client.
+   *
+   * @param string $api_key
+   *   If the API key should be hot swapped.
+   *
+   * @return Client
+   *   The Gemini client.
+   */
+  public function getClient(string $api_key = '') {
+    if ($api_key) {
+      $this->setAuthentication($api_key);
+    }
+
+    $this->loadClient();
+    return $this->client;
+  }
+
+  /**
+   * Loads the Gemini Client with authentication if not initialized.
+   */
+  protected function loadClient(): void {
+    if (!$this->client) {
+      if (!$this->apiKey) {
+        $this->setAuthentication($this->loadApiKey());
+      }
+
+      $this->client = \Gemini::factory()
+        ->withApiKey($this->apiKey)
+        ->withHttpClient($this->httpClient)
+        ->make();
+    }
+  }
+
+  /**
+   * Load API key from key module.
+   *
+   * @return string
+   *   The API key.
+   */
+  protected function loadApiKey(): string {
+    return $this->keyRepository->getKey($this->getConfig()->get('api_key'))
+      ->getKeyValue();
+  }
+
+}
